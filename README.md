9e5c8c49dfe7c70eacac455e086e51607461dc96
jamesdemott <63129048+jamesdemott@users.noreply.github.com>
2020-08-02 16:12:40 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# election_analysis
+# Overview of Audit 
+    Our purpose was to help Seth and Tom submit an election audit to the Colorado Election Board. The election commission requested an audit to determine the qinning candidate and some other data around county participation, total votes both statewide and by county, as well as voter turnout. 
+
+# Election Audit Results 
+    Our analysis found the following: 
+    #Total Statewide Votes were: 369,711
+    #Total county voter participation as a percentage of the whole and total votes were: 
+        Jefferson: 10.5% (38,855)
+        Denver: 82.8% (306,055)
+        Arapahoe: 6.7% (24,801)
+    # Our largest county by vote count was Denver, with 306,055 Votes
+    #Diana DeGette won the election with 73.8% of the vote, or exactly 272,892 votes. 
+
+# Election-Audit Summary
+    In summary, we have constructed a script which can easily be adapted for any election. One example of how this file could be modified would be by incorporating data about how many registered voters there are in a given county. As a separate csv file, this data could be read and iterated upon and then used as a denominator to determine the voter participation, as a percentage of total registered voters to understand turnout. Another way this script could be modified would be to analyze which candidate won each individual county. This could be easily accomplished by setting variables for county_winner and tabulating the variables slightly differently. 
---
+++ deprecatedpypol.py
@@ -0,0 +1,62 @@
+#Total number of votes cast
+#A complete list of candidates who received votes
+#Total number of votes each candidate received
+#Percentage of votes each candidate won
+#The winner of the election based on popular vote
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = ("election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = ("election_analysis.txt")
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate options and candidate votes
+candidate_options = []
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
+#winning candidate tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+# Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
